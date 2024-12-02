86c9adc1aff4e4ae85dde57313e15a49a8947d94
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-12-02 12:20:42 +0300

    in student we initilised the instance, in teacher we used the super in student we appended an empty user list

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+        
+    def learn(self, knowledge_content):
+        self.knowledge.append(knowledge_content)
         pass
